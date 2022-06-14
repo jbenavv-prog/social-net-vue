82c427671c5602b5cbd7de9ea4283c76cd5fa75a
Julian Camilo Benavides Vanegas <julian.benavides@comware.com.co>
2022-06-14 09:31:16 -0500

    Bootstrap added

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+// Vue.use(BootstrapVue);
 
 const app = createApp(App)
 
